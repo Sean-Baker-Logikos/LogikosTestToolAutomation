99d444bf58d0265cf45a169b4bcdf6bd099a351d
Sean Baker <sbaker@logikos.com>
2026-03-10 13:25:27 -0400

    Update to version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="LogikosTestToolAutomation",
-    version="0.1",
+    version="0.0.3",
     include_package_data=True,
     python_requires='>=3.8',
     packages=find_packages(),
